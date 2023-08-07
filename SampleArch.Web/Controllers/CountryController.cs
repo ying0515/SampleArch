debd8da7cf1028826903b6b4fc49e0a19b6b87db
Tony Huang <ying515.huang@msa.hinet.net>
2023-08-07 22:38:07 +0800

    加入專案檔案。

---
+++ SampleArch.Web/Controllers/CountryController.cs
@@ -0,0 +1,28 @@
+﻿using SampleArch.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleArch.Web.Controllers
+{
+    public class CountryController : Controller
+    {
+        //initialize service object
+        ICountryService _CountryService;
+
+        public CountryController(ICountryService countryService)
+        {
+            _CountryService = countryService;
+        }
+
+        // GET: Country
+        public ActionResult Index()
+        {
+            var data = _CountryService.GetAll();
+            var aa = data.ToList();
+            return View();
+        }
+    }
+}
---
+++ SampleArch.Web/Module/EFModule.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using SampleArch.Model;
+using SampleArch.Repository;
+using SampleArch.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SampleArch.Web.Module
+{
+    public class EFModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new RepositoryModule());
+
+            builder.RegisterType(typeof(SampleArchContext)).As(typeof(DbContext)).InstancePerLifetimeScope();
+            builder.RegisterType(typeof(UnitOfWork)).As(typeof(IUnitOfWork)).InstancePerRequest();
+
+        }
+    }
+}
